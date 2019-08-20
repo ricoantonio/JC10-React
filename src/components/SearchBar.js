109e44f01ffa05584cad25bc71a77fcb9b5cbac7
ricoantonio <ricoantonio33@hotmail.com>
2019-08-20 15:36:47 +0700

    paxios

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,17 @@ import React, {Component} from "react"
 
 class SearchBar extends Component{
 
+    state={
+        keyword:""
+    }
+
     onSubmitForm=(e)=>{
         e.preventDefault()
+    }
+
+    onChangeText=(e)=>{
+        // Menyimpan text dari user di state.keyword
+        this.setState({keyword: e.target.value}) 
     }
 
     render() {
@@ -12,7 +21,7 @@ class SearchBar extends Component{
                 <h3 className="text-center mt-3">Search Image</h3>
                 <form className="form-group mt-5 row" onSubmit={this.onSubmitForm}>
                     <input className="form-control btn-light" 
-                        onChange={ (e)=>{console.log(e.target.value)} }
+                        onChange={this.onChangeText}
                         placeholder="search..." 
                         type="text"/>
                 </form>
@@ -25,6 +34,8 @@ export default SearchBar
 
 
 // onSubmit, ketika text input diEnter
-// e.preventDefault() akan menghentikan halaman agar tidak refresh
+    // e.preventDefault() akan menghentikan halaman agar tidak refresh
 // onChange, ketika ada perubahan di tag input text
-// event.target.value, adalah property yang berisi teks yang kita ketik 
+    // event.target.value, adalah property yang berisi teks yang kita ketik 
+// this.setState merupakan finction untuk mengubah data pada state
+    // setState akan memnerima saru buah parameter yaitu object{}
